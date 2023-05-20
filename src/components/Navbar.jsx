cc2d8047c50d101c46c78bae1c9a12d862367ce8
Le Van Quoc <quoc020721@gmail.com>
2023-05-20 10:51:50 +0700

    edit code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ const Navbar = () => {
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{" "}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                LeVanQuoc46997
+                LeVanQuoc=
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
